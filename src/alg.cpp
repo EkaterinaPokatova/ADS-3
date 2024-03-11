f5023b262a7007c1bcc094e1057fec9c353c9839
Ekaterina Pokatova <150853636+EkaterinaPokatova@users.noreply.github.com>
2024-03-12 00:10:06 +0300

    Pokatova

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,13 @@
 // Copyright 2021 NNTU-CS
 
+#include <cstdint>
+
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    uint64_t ValueCounter = 0;
+    for (uint64_t i = 0; i < size; i++) {
+        if (arr[i] == value) {
+            ValueCounter++;
+        }
+    }
+    return ValueCounter;
 }
